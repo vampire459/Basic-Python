8a74a20662e36327fc9eddcb7da3805f9630216b
Arnab Jana <101020044+vampire459@users.noreply.github.com>
2023-04-27 02:54:33 +0530

    Update classroom.py

--- classroom.py
+++ classroom.py
@@ -1,7 +1,7 @@
 from student import student 
 
 stu1 = student("Arnab","Computer Science and Enginering",9.46,False)
-stu2 = student("Tanisha","Computer Science",4,True)
+stu2 = student("Soumyadep","Computer Science",4,True)
 
 print(stu1.name)
 print(stu2.major)
